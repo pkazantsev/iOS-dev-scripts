60136d06d5398bffb50fbc16ffeac8040cc46fa5
Pavel Kazantsev <pv.kazantsev@gmail.com>
2019-01-18 11:33:52 +0300

    Updated xcode_todo.sh
    Include Objective-C files and exclude CocoaPods and Carthage folders.

--- Scripts/xcode_todo.sh
+++ Scripts/xcode_todo.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
-TAGS="TODO:|FIXME:"  
-echo "searching ${SRCROOT} for ${TAGS}"  
-find "${SRCROOT}" -name "*.swift" -print0 \
-	| xargs -0 egrep --with-filename --line-number --only-matching "($TAGS).*$" \
-	| perl -p -e "s/($TAGS)/ warning: $1/"
+TAGS="TODO:|FIXME:"
+echo "Searching ${SRCROOT} for ${TAGS}"
+find "${SRCROOT}" \( -path "${SRCROOT}/Pods" -o -path "${SRCROOT}/Carthage" \) -prune -o \( -name "*.h" -or -name "*.m" -or -name "*.swift" \) -print0 \
+	| xargs -0 egrep --with-filename --line-number --only-matching "($TAGS).*\$" \
+	| perl -p -e "s/($TAGS)/ warning: \$1/"
